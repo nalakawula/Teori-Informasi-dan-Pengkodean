90eeaf976cfad62f53d7da504a06339faff5f342
sumarouno <sumarsono.wongbandar@gmail.com>
2017-05-23 18:39:55 +0700

    :pencil2: backup to git for sharing

---
+++ 5301413057_Hamming_code/main.m
@@ -0,0 +1,10 @@
+% Simulasi Hamming Code menggunakan simulink matlab
+% 5301413057 Sumarsono
+% model dibuat menggunakan simulink
+% kemudian dijalankan menggunakan m file
+% Buka matlab
+% Pada console window ketik main kemudian tekan enter
+% Tunggu beberapa saat sampai jendela simulink terbuka
+% Setelah terbuka, simulasi siap dijalankan
+
+open('SUMARSONO_Simulink_Hamming_Code');
